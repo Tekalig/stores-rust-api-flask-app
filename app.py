7288e67ba31804c559eb0eb77b29bf459958e1a4
Tekalig <tekahazi06@gmail.com>
2025-02-10 11:53:25 +0300

    create get method for store

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+stores =[ {
+    "name":"Mystore",
+    "items":[{
+        "name":"chair",
+        "price":"$25"
+    }]
+}]
+
+@app.get("/store")
+def get_store():
+    return {"stores":stores}
