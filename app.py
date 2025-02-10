534a295fc62caa1d6dd196069c69a5e8f56f30c1
Tekalig <tekahazi06@gmail.com>
2025-02-10 11:54:40 +0300

    create post method for store to add item to specific store

--- app.py
+++ app.py
@@ -22,3 +22,13 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
+@app.post("/store/<string:name>/item")
+def add_item(name):
+    req = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name":req["name"], "price":req["price"]}
+            store["items"].append(new_item)
+            return new_item,201
+    return {"message": "Store isn't Found!"}, 404
+
