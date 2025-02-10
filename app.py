cb6457af4586450eebdb8853d71f7b7dbc51c8f2
Tekalig <tekahazi06@gmail.com>
2025-02-10 11:53:52 +0300

    create post method for store

--- app.py
+++ app.py
@@ -14,3 +14,11 @@ stores =[ {
 @app.get("/store")
 def get_store():
     return {"stores":stores}
+
+@app.post("/store")
+def create_store():
+    req = request.get_json()
+    new_store = {"name":req["name"], "items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
