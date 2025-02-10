9a38113f2ee01de781d6036a388c5790802e69fe
Tekalig <tekahazi06@gmail.com>
2025-02-10 15:23:41 +0300

    debug the error which happen in the test endpoint

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import uuid
-from pyexpat.errors import messages
-
-from flask import Flask, request
+from flask import Flask, request, message_flashed
 from flask_smorest import abort
 from db import stores, items
 app = Flask(__name__)
@@ -14,7 +12,14 @@ def get_store():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
-    store_id = uuid.uuid4()
+    if "name" not in store_data:
+        abort(http_status_code=400, message_flashed="Bad request, Ensure name field include",)
+
+    for store in stores:
+        if store_data["name"] == store["name"]:
+            abort(http_status_code=400, message="Store already exists",)
+
+    store_id = uuid.uuid4().hex
     store = {**store_data, "id":store_id}
     stores[store_id] = store
     return store, 201
@@ -23,16 +28,16 @@ def create_store():
 def create_item():
     item_data = request.get_json()
     if "store_id" not in item_data or "price" not in item_data or "name" not in item_data:
-        abort(http_status_code=400, message="Bad request, price, name, and store_id should be include.")
+        abort(http_status_code=400, message="Bad request, price, name, and store_id should be include.",)
 
     for item in items:
         if item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]:
-            abort(http_status_code=400, message="Item already exists")
+            abort(http_status_code=400, message="Item already exists",)
 
     if item_data["store_id"] not in stores:
-        abort(http_status_code=404, message="Store isn't Found!")
+        abort(http_status_code=400, message="Store isn't Found!",)
 
-    item_id = uuid.uuid4()
+    item_id = uuid.uuid4().hex
     item = {**item_data, "id":item_id}
     items[item_id] = item
     return item, 201
@@ -42,7 +47,7 @@ def get_specific_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        abort(http_status_code=404, message="Store isn't Found!")
+        abort(http_status_code=400, message="Store isn't Found!",)
 
 @app.get("/items")
 def get_items():
@@ -53,4 +58,4 @@ def get_specific_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        abort(http_status_code=404, message="Item isn't Found!")
+        abort(http_status_code=400, message="Item isn't Found!",)
