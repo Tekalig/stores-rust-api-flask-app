95429e4e7ea20ffed210b98d9e33616633face09
Tekalig <tekahazi06@gmail.com>
2025-02-13 20:35:52 +0300

    update schemas.py to have tag and item table combined

--- schemas.py
+++ schemas.py
@@ -5,11 +5,6 @@ class PlainItemSchema(Schema):
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
-class ItemUpdateSchema(Schema):
-    name = fields.Str()
-    price = fields.Float()
-    store_id = fields.Int()
-
 class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
@@ -18,16 +13,29 @@ class PlainTagSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
+
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+    store_id = fields.Int()
+
 class StoreUpdateSchema(Schema):
     name = fields.Str()
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    stores = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(dump_only=True, load_only=True)
-    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    stores = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    tags = fields.Nested(TagSchema)
+    items = fields.Nested(ItemSchema)
